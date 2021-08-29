6ae8ea3ef4f089c6491763e9c1459a14d06fffc8
wipawan <wipawan-vat63@tbs.tu.ac.th>
2021-08-29 16:46:11 +0700

    Add 4 pages

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -10,9 +10,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.amber,
+        accentColor: Colors.red,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.purple),
+        ),
       ),
-      home: MyHomePage(title: 'MhaMeow Home Page'),
+      initialRoute: '/forth',
+      routes: <String, WidgetBuilder>{
+        '/first': (context) => FirstPage(),
+        '/second': (context) => SecondPage(),
+        '/third': (context) => ThirdPage(),
+        '/forth': (context) => ForthPage(),
+      },
     );
   }
 }
@@ -30,17 +40,17 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Image cat = Image.asset(
     'assets/cat1.png',
-    width: 120,
+    width: 200,
   );
 
   Image cat1 = Image.asset(
     'assets/cat1.png',
-    width: 120,
+    width: 200,
   );
 
   Image cat2 = Image.asset(
     'assets/cat2.png',
-    width: 120,
+    width: 200,
   );
 
   void _incrementCounter() {
@@ -67,7 +77,16 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            cat,
+            Container(
+              height: 200.0,
+              margin: EdgeInsets.only(left: 100.0, right: 100.0, bottom: 20.0),
+              padding: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.amber.withOpacity(0.25),
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+              child: cat,
+            ),
             Text(
               'You have pushed the button this many times:',
             ),
@@ -112,6 +131,177 @@ class SubmitButton extends StatelessWidget {
       onPressed: () {
         print('pressing....');
       },
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Page'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.arrow_forward)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.architecture)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.face)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.food_bank)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.electric_car)),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  get mainAxisAlignment => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.build_sharp),
+        onPressed: () {},
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Break!!! Back at 7.40pm',
+            ),
+            Table(
+              children: [
+                TableRow(
+                  children: [
+                    Container(
+                      child: Center(child: Text('No.')),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    Container(
+                      child: Center(child: Text('Name')),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    Container(
+                      child: Center(child: Text('Gender')),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    Text('1'),
+                    Text('Mhameow'),
+                    Text('Male'),
+                  ],
+                ),
+                TableRow(children: [
+                  Text('2'),
+                  Text('nui'),
+                  Text('Female'),
+                ]),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Third Page'),
+          centerTitle: true,
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.cloud),
+              ),
+              Tab(
+                icon: Icon(Icons.beach_access_outlined),
+              ),
+              Tab(
+                icon: Icon(Icons.brightness_1_outlined),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(
+              child: Text('Cloud'),
+            ),
+            Center(
+              child: Text('Umbrella'),
+            ),
+            Center(
+              child: Text('Sunny'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ForthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<String> entries = <String>[
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N'
+    ];
+    final List<int> colorCodes = <int>[600, 500, 100];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ListView Example'),
+      ),
+      body: ListView.separated(
+        padding: EdgeInsets.all(8.0),
+        itemCount: entries.length,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 50,
+            color: Colors.amber[colorCodes[index % 3]],
+            child: Center(
+              child: Text('Enter ${entries[index]}'),
+            ),
+          );
+        },
+        separatorBuilder: (context, index) => Divider(),
+      ),
     );
   }
 }
